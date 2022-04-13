3b748bd5e2ddf1373e45fe4df45d0f50707bc977
Bruno Santos <brcesar19@gmail.com>
2022-04-13 10:38:46 -0300

    build: refactor and clean files

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,5 @@ import { HttpModule } from './http/http.module';
 
 @Module({
   imports: [DatabaseModule, ConfigModule, HttpModule],
-  controllers: [],
-  providers: [],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
+import { version } from '../package.json';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
-  app.listen(3000).then(() => console.log('[cas-admin-api] Running app'));
+  app.listen(3000).then(() => Logger.log(`[cas-admin-api][v${version}] Running app`));
 }
 bootstrap();
