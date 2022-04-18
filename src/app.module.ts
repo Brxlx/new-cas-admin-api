beace52c2595103d2a9e9d2ef4645adc67027906
Bruno Santos <brcesar19@gmail.com>
2022-04-18 16:55:08 -0300

    chore: remove unecessary database module on app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 
 import { ConfigModule } from './config/config.module';
-import { DatabaseModule } from './database/database.module';
 import { HttpModule } from './infra/http/http.module';
 
 @Module({
-  imports: [DatabaseModule, ConfigModule, HttpModule],
+  imports: [ConfigModule, HttpModule],
 })
 export class AppModule {}
